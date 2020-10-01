c70ca25748f6752b61966ad72843460935c5df33
SpiderCallous <72211923+SpiderCallous@users.noreply.github.com>
2020-10-01 20:26:14 +0530

    Create ReturnFunction.py

---
+++ ReturnFunction.py
@@ -0,0 +1,13 @@
+def absolute_value(num):
+    """This function returns the absolute
+    value of the entered number"""
+
+    if num >= 0:
+        return num
+    else:
+        return -num
+
+
+print(absolute_value(2))
+
+print(absolute_value(-4))
