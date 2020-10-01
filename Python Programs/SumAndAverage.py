6fdd40d452c3540373e39444813776ad8a6de57b
SpiderCallous <72211923+SpiderCallous@users.noreply.github.com>
2020-10-01 20:06:44 +0530

    Create SumAndAverage.py

---
+++ Python Programs/SumAndAverage.py
@@ -0,0 +1,5 @@
+n = 10
+sum = n * (n+1) / 2
+average  = ( n * (n+1) / 2) / n
+print("Sum of the first ", n, "natural numbers using formula is: ", sum )
+print("Average of the first ", n, "natural numbers using formula is: ", average )
