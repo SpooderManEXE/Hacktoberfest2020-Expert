b3172da752814821dd21fd79c45444fbbf0d51b0
Sulthan Nizarudin <n.sultan001@gmail.com>
2020-10-02 17:30:28 +0530

    update

--- C Programs/Palindrome.c
+++ C Programs/Palindrome.c
@@ -1,3 +1,4 @@
+//Program to check palindrome
 #include <stdio.h>
 int main() {
     int n, reversedN = 0, remainder, originalN;
@@ -5,14 +6,14 @@ int main() {
     scanf("%d", &n);
     originalN = n;
 
-    
+    // reversed integer is stored in reversedN
     while (n != 0) {
         remainder = n % 10;
         reversedN = reversedN * 10 + remainder;
         n /= 10;
     }
 
-    
+    // palindrome if orignalN and reversedN are equal
     if (originalN == reversedN)
         printf("%d is a palindrome.", originalN);
     else
