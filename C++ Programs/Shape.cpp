9485ebd08afe5eea5f0d6a376c2d3b627e3c293a
Sumeeha-Ashraf <72332068+Sumeeha-Ashraf@users.noreply.github.com>
2020-10-04 13:24:21 +0530

    Create Shape.cpp

---
+++ C++ Programs/Shape.cpp
@@ -0,0 +1,67 @@
+#include<iostream.h>
+using namespace std;
+int main()
+{
+ cout<<"\n\n\n\n\n";
+ 
+ for(int i=0;i<15;i++)
+ cout<<" ";
+ cout<<"*         *";
+ cout<<endl;
+ 
+ for(int j=0;j<13;j++)
+ cout<<" ";
+ cout<<"*   *     *   *";
+ cout<<endl;
+
+ for(int k=0;k<11;k++)
+ cout<<" ";
+ cout<<"*      *   *      *";
+ cout<<endl;
+
+ for(int l=0;l<11;l++)
+ cout<<" ";
+ cout<<"*       * *       *";
+ cout<<endl;
+ 
+ for(int m=0;m<11;m++)
+ cout<<" ";
+ cout<<"*        *        *";
+ cout<<endl;
+ 
+ for(int n=0;n<11;n++)
+ cout<<" ";
+ cout<<"*                 *";
+ cout<<endl;
+ 
+ for(int o=0;o<12;o++)
+ cout<<" ";
+ cout<<"*               *";
+ cout<<endl; 
+
+ for(int p=0;p<13;p++)
+ cout<<" ";
+ cout<<"*             *";
+ cout<<endl;
+ 
+ for(int q=0;q<14;q++)
+ cout<<" ";
+ cout<<"*           *";
+ cout<<endl;
+ 
+ for(int r=0;r<15;r++)
+ cout<<" ";
+ cout<<"*         *";
+ cout<<endl;
+ 
+ for(int s=0;s<17;s++)
+ cout<<" ";
+ cout<<"*     *";
+ cout<<endl;
+ 
+ for(int t=0;t<20;t++)
+ cout<<" ";
+ cout<<"**";
+ cout<<endl;
+ return 0;
+}
