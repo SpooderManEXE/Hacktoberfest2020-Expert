aa9f7443000e457be25cd1d1deeff8877abc7c39
TheRealMadScientist <56930337+TheRealMadScientist@users.noreply.github.com>
2020-10-04 17:57:31 +0530

    Create compare3num.py

---
+++ Python Programs/compare3num.py
@@ -0,0 +1,12 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+num3 = float(input("Enter third number: "))
+ 
+if (num1 > num2) and (num1 > num3):
+   largest = num1
+elif (num2 > num1) and (num2 > num3):
+   largest = num2
+else:
+   largest = num3
+ 
+print("The largest number is",largest)
