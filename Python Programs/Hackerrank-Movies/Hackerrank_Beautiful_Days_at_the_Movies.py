4040e7d968e718c8a9edb751e08fae3191122b4d
Tanishka Gupta <gtanishka8@gmail.com>
2020-10-07 19:30:02 +0530

    please accept my commit

--- Python Programs/Hackerrank-Movies/Hackerrank_Beautiful_Days_at_the_Movies.py
+++ Python Programs/Hackerrank-Movies/Hackerrank_Beautiful_Days_at_the_Movies.py
@@ -1,43 +1,13 @@
-#!/bin/python3
-
-import math
-
-import os
-
-import random
-
-import re
-
-import sys
-
-from decimal import *
-
-# Complete the beautifulDays function below.
-
-def beautifulDays(i, j, k):
-
-    count=0
-
-    for l in range(i,j+1):
-
-        n = (str(l)[::-1])
-
-        if(abs(l-int(n))%k ==0):
-
-            count+=1
-
-    return count
-
-if __name__ == '__main__':
-
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    ijk = input().split()
-
-    i = int(ijk[0])
-
-    j = int(ijk[1])
-
-    k = int(ijk[2])
-
-    result = beautifulDays(i, j, k)
+ijk = input().split()
+a = int(ijk[0])
+b = int(ijk[1])
+k = int(ijk[2])
+li = [i for i in range(a,b+1)]
+lid = list(map(str,li))
+lis = []
+count = 0
+for i in range(len(lid)):
+    lis.append(int(''.join(list(reversed(lid[i])))))
+    if(abs(int(lid[i])-lis[i])%k==0):
+        count += 1
+print(count)
