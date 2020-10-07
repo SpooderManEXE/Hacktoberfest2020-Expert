a7889025570d6ad272fefde1e42ff4af8a18a3f9
Tanishka Gupta <gtanishka8@gmail.com>
2020-10-07 19:21:03 +0530

    please accept the following edits

--- Python Programs/ArmstrongNumber.py
+++ Python Programs/ArmstrongNumber.py
@@ -1,22 +1,11 @@
-Number = int(input("\nPlease Enter the Number to Check for Armstrong: "))
-
-# Initializing Sum and Number of Digits
-Sum = 0
-Times = 0
-           
-# Calculating Number of individual digits
-Temp = Number
-while Temp > 0:
-           Times = Times + 1
-           Temp = Temp // 10
-
-# Finding Armstrong Number
-Temp = Number
-while Temp > 0:
-           Reminder = Temp % 10
-           Sum = Sum + (Reminder ** Times)
-           Temp //= 10
-if Number == Sum:
-           print("\n %d is Armstrong Number.\n" %Number)
+no = int(input())
+sum = 0
+temp = no
+while temp > 0:
+    digit = temp%10
+    sum += digit**3
+    temp //= 10
+if no == sum:
+    print(no, 'is armstrong number')
 else:
-           print("\n %d is Not a Armstrong Number.\n" %Number)
+    print('try again')
