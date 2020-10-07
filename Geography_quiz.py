15134751ee87a0fee106c89309d31ccbf3f9703a
master77229 <72162459+master77229@users.noreply.github.com>
2020-10-07 16:38:51 +0530

    Create Geography_quiz.py

---
+++ Geography_quiz.py
@@ -0,0 +1,44 @@
+import random
+
+class QA:
+  def __init__(self, question, correctAnswer, otherAnswers):
+    self.question = question
+    self.corrAnsw = correctAnswer
+    self.otherAnsw = otherAnswers
+
+qaList = [QA("Where is New Delhi?", "in India", ["in Pakistan", "in China"]),
+QA("What is the capital of Angola?", "Luanda", ["Sydney", "New York", "Angola doesn't exist"]),
+QA("Which of the following is not in Europe?", "South Africa", ["England", "Spain", "Germany"]),
+QA("Which of the following is not a capital of South Africa?", "Durban", ["Pretoria", "Cape Town"]),
+QA("Which of the following is not an African country?", "Malaysia", ["Madagascar", "Djibouti", "South Africa", "Zimbabwe"])]
+
+corrCount = 0
+random.shuffle(qaList)
+for qaItem in qaList:
+  print(qaItem.question)
+  print("Possible answers are:")
+  possible = qaItem.otherAnsw + [qaItem.corrAnsw] 
+  random.shuffle(possible)
+  count = 0 
+  while count < len(possible):
+    print(str(count+1) + ": " + possible[count])
+    count += 1
+  print("Please enter the number of your answer:")
+  userAnsw = input()
+  while not userAnsw.isdigit():
+    print("That was not a number. Please enter the number of your answer:")
+    userAnsw = input()
+  userAnsw = int(userAnsw)
+  while not (userAnsw > 0 and userAnsw <= len(possible)):
+    print("That number doesn't correspond to any answer. Please enter the number of your answer:")
+    userAnsw = input()
+  if possible[userAnsw-1] == qaItem.corrAnsw:
+    print("Your answer was correct.")
+    corrCount += 1
+  else:
+    print("Your answer was wrong.")
+    print("Correct answer was: " + qaItem.corrAnsw)
+  print("")
+
+print("You answered " + str(corrCount) + " of " + str(len(qaList)) + " questions correctly.")
+
