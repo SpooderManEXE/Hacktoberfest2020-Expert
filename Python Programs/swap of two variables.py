2080053abf1e8ad20830d13aca61355864c67926
rsgsaad <63123740+rsgsaad@users.noreply.github.com>
2020-10-16 00:16:55 +0530

    Update with pythonist style swap varible

--- Python Programs/swap of two variables.py
+++ Python Programs/swap of two variables.py
@@ -8,9 +8,7 @@ y = 10
 #y = input('Enter value of y: ')
 
 # create a temporary variable and swap the values
-temp = x
-x = y
-y = temp
+x,y=y,x
 
 print('The value of x after swapping: {}'.format(x))
 print('The value of y after swapping: {}'.format(y))
