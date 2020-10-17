ecd42b9ba1344d59ec84d3a4e076234bf4e43e72
Thibauld Dujardin <22295595+ThibDujardin@users.noreply.github.com>
2020-10-17 19:42:54 +0200

    Update CreateAList.java

--- Java Programs/CreateAList.java
+++ Java Programs/CreateAList.java
@@ -3,9 +3,12 @@ import java.util.*;
 public class CreateAList{
 
      public static void main(String []args){
+         List<String> list1 = List.of("Hello", "World", "!");
          List<String> list = new ArrayList<>();
          list.add("Hello");
          list.add("World!");
+          
          System.out.println(Arrays.toString(list.toArray()));
+         System.out.println(Arrays.toString(list1.toArray()));
      }
 }
