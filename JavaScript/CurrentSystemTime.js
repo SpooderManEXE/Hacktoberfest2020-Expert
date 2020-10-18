c06eed769c2bbee13777966ecaf938e8415b4ec6
Rohan Singh Rawat <51116846+rohansinghrawat99@users.noreply.github.com>
2020-10-18 19:08:12 +0530

    CURRENT SYSTEM TIME in JAVASCRIPT

---
+++ JavaScript/CurrentSystemTime.js
@@ -0,0 +1,7 @@
+var currentdate = new Date(); 
+var datetime = "Last Sync: " + currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + " @ "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
