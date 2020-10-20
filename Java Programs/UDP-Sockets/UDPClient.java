f230069b92b0e7c8adf3dea3a9ac5270574af6b1
Shalini N <31442996+Shalini-CEG@users.noreply.github.com>
2020-10-20 14:52:14 +0530

    Create UDPClient.java

---
+++ Java Programs/UDP-Sockets/UDPClient.java
@@ -0,0 +1,29 @@
+
+import java.net.*;
+import java.util.Scanner;
+// Java Socket Connections - Client
+public class UdpClient {
+    public static void main(String args[]) throws Exception {
+        System.out.println("Write a message: ");
+        DatagramSocket ds = new DatagramSocket();
+        Scanner in = new Scanner(System.in);
+
+        //		Send
+        String outStr = in.nextLine();
+        byte[] outByte = outStr.getBytes();
+        InetAddress address = InetAddress.getLocalHost();
+        int port = 9875;
+        DatagramPacket dpSend = new DatagramPacket(outByte, outByte.length, address, port);
+        ds.send(dpSend);
+
+        //Receive
+        byte[] inByte = new byte[1024];
+        DatagramPacket dpReceive = new DatagramPacket(inByte, inByte.length);
+        ds.receive(dpReceive);
+        String inStr = new String(dpReceive.getData());
+        System.out.println(inStr);
+
+        in.close();
+        ds.close();
+    }
+}
