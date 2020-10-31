e6d59983f19781d8bb00cbb71fa28227f6f4a5a2
iceee124 <73751786+iceee124@users.noreply.github.com>
2020-10-31 23:17:33 +0530

    Create areaofrectangle.py

---
+++ Python Programs/areaofrectangle.py
@@ -0,0 +1,13 @@
+# Python Program to find Area of a Rectangle
+
+width = float(input('Please Enter the Width of a Rectangle: '))
+height = float(input('Please Enter the Height of a Rectangle: '))
+
+# calculate the area
+Area = width * height
+
+# calculate the Perimeter
+Perimeter = 2 * (width + height)
+
+print("\n Area of a Rectangle is: %.2f" %Area)
+print(" Perimeter of Rectangle is: %.2f" %Perimeter)
