9f052c626270670369f77d476f4cb160e05361fb
Gowrishankar V.V <89328469+Gowrishankarvv@users.noreply.github.com>
2021-10-09 11:41:03 +0530

    Create ascii.c

---
+++ C Programs/ascii.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+ int main() {
+   char c;
+   printf("https://github.com/Gowrishankarvv");              
+   printf("Enter a character: ");
+   scan("%c" , &c);
+   print("ASSII value =%d, c);
+         return 0;
+ }        
+         /* ASCII value PROGRAM */
+ /* code by gowrishankarvv */
